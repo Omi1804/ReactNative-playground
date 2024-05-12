98b1e54818b8e47c196b1cd1c5ef8ec18132344e
Om Nigam <om2021064@akgec.ac.in>
2024-05-12 15:28:34 +0530

    first commit

---
+++ AppPro.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+
+const AppPro = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark'; //this is the way to figure out is the phone is in dark mode or light mode
+
+  return (
+    <View style={styles.containers}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        Hello World
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containers: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+export default AppPro;
