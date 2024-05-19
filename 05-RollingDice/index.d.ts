2156c744feeb5a57fa16f73f71ce1e03a71de292
Om Nigam <om2021064@akgec.ac.in>
2024-05-19 18:16:35 +0530

    Starting Project 5

---
+++ 05-RollingDice/index.d.ts
@@ -0,0 +1,4 @@
+//this is created by this name only
+//this is created so that when we declare
+//somethign and do not use it then typescript starts giving errors so it will shutdown those errors
+declare module '*.png';
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './04-BgChanger/App';
+import App from './05-RollingDice/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
