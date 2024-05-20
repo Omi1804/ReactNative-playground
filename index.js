d85409c7ebd065ce22c69b5fa5101364909010f0
Om Nigam <om2021064@akgec.ac.in>
2024-05-20 10:10:10 +0530

    finished project 6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './05-RollingDice/App';
+import App from './06-currencyConverter/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
