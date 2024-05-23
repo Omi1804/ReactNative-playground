50f58e9c234f766bf821fd7ee248c9001ecaced4
Om Nigam <om2021064@akgec.ac.in>
2024-05-23 16:53:56 +0530

    starting project Spotify clone

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './07-ticktactoe/App';
+import App from './08-SpotifyClone/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
