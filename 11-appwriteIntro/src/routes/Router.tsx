7bfb20a01b1da0e334bc0681da28d2c3fca39486
Om Nigam <om2021064@akgec.ac.in>
2024-06-12 10:10:10 +0530

    adding more routers logics

--- 11-appwriteIntro/src/routes/Router.tsx
+++ 11-appwriteIntro/src/routes/Router.tsx
@@ -1,12 +1,26 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {AppwriteContext} from '../appwrite/AppwriteContext';
+import Loading from '../components/Loading';
+
+//Routes
+import AppStack from './AppStack';
+import AuthStack from './AuthStack';
 
 export default function Router() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const {appwrite, isLoggedIn, setIsLoggedIn} = useContext(AppwriteContext);
+
+  useEffect(() => {
+    appwrite;
+  }, [appwrite, setIsLoggedIn]);
+
   return (
     <View>
       <Text>Router</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
