be448c9979aab5ea065e9afa81117289ba6b3df5
Om Nigam <om2021064@akgec.ac.in>
2024-06-14 10:10:10 +0530

    configuring home page

--- 11-appwriteIntro/src/screens/Home.tsx
+++ 11-appwriteIntro/src/screens/Home.tsx
@@ -7,6 +7,8 @@ import Snackbar from 'react-native-snackbar';
 
 //context API
 import {AppwriteContext} from '../appwrite/AppwriteContext';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Image} from '@rneui/base';
 
 type UserObj = {
   name: string;
@@ -27,15 +29,79 @@ const Home = () => {
     });
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    appwrite
+      .getCurrentUser()
+      .then(resp => {
+        if (resp) {
+          const user: UserObj = {
+            name: resp.name,
+            email: resp.email,
+          };
+
+          setUserData(user);
+        }
+      })
+      .catch();
+  }, [appwrite]);
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.welcomeContainer}>
+        <Image
+          source={{
+            uri: 'https://appwrite.io/images-ee/blog/og-private-beta.png',
+            width: 400,
+            height: 300,
+            cache: 'default',
+          }}
+          resizeMode="contain"
+        />
+        <Text style={styles.message}>
+          Build Fast. Scale Big. All in One Place.
+        </Text>
+        {userData && (
+          <View style={styles.userContainer}>
+            <Text style={styles.userDetails}>Name: {userData.name}</Text>
+            <Text style={styles.userDetails}>Email: {userData.email}</Text>
+          </View>
+        )}
+      </View>
+      <FAB
+        placement="right"
+        color="#f02e65"
+        size="large"
+        title="Logout"
+        icon={{name: 'logout', color: '#FFFFFF'}}
+        onPress={handleLogout}
+      />
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B0D32',
+  },
+  welcomeContainer: {
+    padding: 12,
+
+    flex: 1,
+    alignItems: 'center',
+  },
+  message: {
+    fontSize: 26,
+    fontWeight: '500',
+    color: '#FFFFFF',
+  },
+  userContainer: {
+    marginTop: 24,
+  },
+  userDetails: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+});
