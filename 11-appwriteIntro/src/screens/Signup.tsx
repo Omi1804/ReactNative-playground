f71711108e481b2b47e7e776b4665b9889fdf525
Om Nigam <om2021064@akgec.ac.in>
2024-06-15 10:10:10 +0530

    configuring signup page

--- 11-appwriteIntro/src/screens/Signup.tsx
+++ 11-appwriteIntro/src/screens/Signup.tsx
@@ -1,7 +1,63 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useContext, useState} from 'react';
+
+//react native styling elements
+import {FAB} from '@rneui/themed';
+import Snackbar from 'react-native-snackbar';
+
+//context API
+import {AppwriteContext} from '../appwrite/AppwriteContext';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Image} from '@rneui/base';
+
+//navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {AuthStackParamList} from '../routes/AuthStack';
+
+type SignupScreenProps = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const Signup = () => {
+  const {appwrite, setIsLoggedIn} = useContext(AppwriteContext);
+  const [error, setError] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+
+  const handleSignup = () => {
+    if (
+      name.length < 1 ||
+      email.length < 1 ||
+      password.length < 1 ||
+      repeatPassword.length < 1
+    ) {
+      setError('All fields are required');
+    } else if (password !== repeatPassword) {
+      setError('Passwords do not match');
+    } else {
+      const user = {
+        email,
+        password,
+        name,
+      };
+      appwrite
+        .createAccout(user)
+        .then(res => {
+          if (res) {
+            setIsLoggedIn(true);
+            Snackbar.show({
+              text: 'Signup success',
+              duration: Snackbar.LENGTH_SHORT,
+            });
+          }
+        })
+        .catch(e => {
+          console.log(e);
+          setError(e.message);
+        });
+    }
+  };
+
   return (
     <View>
       <Text>Signup</Text>
